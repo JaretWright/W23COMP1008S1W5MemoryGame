12709913135689e1f1e6e962551945aeef1eec59
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-09 16:43:30 -0500

    Week 5 - Updated the view with objects and fx:id's

--- src/main/java/com/example/w23comp1008s1w5memorygame/MemoryGameController.java
+++ src/main/java/com/example/w23comp1008s1w5memorygame/MemoryGameController.java
@@ -1,4 +1,24 @@
 package com.example.w23comp1008s1w5memorygame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
 public class MemoryGameController {
+
+    @FXML
+    private Label correctLabel;
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private Label guessesLabel;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
 }
