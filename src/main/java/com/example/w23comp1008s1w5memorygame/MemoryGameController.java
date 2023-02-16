2cebf71e617c1664da8f1805e3b51835219ef7d3
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-02-16 17:35:56 -0500

    Week 6 - Added a method to load the new war-game.fxml file

--- src/main/java/com/example/w23comp1008s1w5memorygame/MemoryGameController.java
+++ src/main/java/com/example/w23comp1008s1w5memorygame/MemoryGameController.java
@@ -2,12 +2,17 @@ package com.example.w23comp1008s1w5memorygame;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -159,7 +164,15 @@ public class MemoryGameController implements Initializable {
      * @param event
      */
     @FXML
-    void playWarGame(ActionEvent event) {
+    void playWarGame(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("war-game.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
+        //get the Stage object from the ActionEvent
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setTitle("War Game");
+        stage.setScene(scene);
+        stage.show();
     }
 }
