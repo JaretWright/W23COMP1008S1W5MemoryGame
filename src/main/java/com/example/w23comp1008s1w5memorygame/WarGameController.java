42db45b23312a863877cc1734b6f7737a99f6414
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-23 17:36:58 -0500

    Week 7 - added logic to make the winner a more pronounced experience.

--- src/main/java/com/example/w23comp1008s1w5memorygame/WarGameController.java
+++ src/main/java/com/example/w23comp1008s1w5memorygame/WarGameController.java
@@ -3,6 +3,7 @@ package com.example.w23comp1008s1w5memorygame;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
@@ -22,12 +23,18 @@ public class WarGameController implements Initializable {
     private Label p2CardCountLabel;
 
     @FXML
+    private Label winnerLabel;
+
+    @FXML
     private ImageView p2ImageView;
+
+    @FXML
+    private Button playButton;
 
     private ArrayList<Card> p1Hand, p2Hand, cardPile;
 
     @FXML
-    void playHand() {
+    private void playHand() {
         p1ImageView.setVisible(true);
         p2ImageView.setVisible(true);
 
@@ -38,35 +45,75 @@ public class WarGameController implements Initializable {
         else if (p2Hand.size()==0)
             declareWinner("Player 1");
 
-        //P1 removes top card and adds to pile
-        Card p1Card = p1Hand.remove(0);
-        cardPile.add(p1Card);
-        p1ImageView.setImage(p1Card.getImage());
+        else {
+            //P1 removes top card and adds to pile
+            Card p1Card = p1Hand.remove(0);
+            cardPile.add(p1Card);
+            p1ImageView.setImage(p1Card.getImage());
 
-        //P2 removes top card and adds to the pile
-        Card p2Card = p2Hand.remove(0);
-        cardPile.add(p2Card);
-        p2ImageView.setImage(p2Card.getImage());
+            //P2 removes top card and adds to the pile
+            Card p2Card = p2Hand.remove(0);
+            cardPile.add(p2Card);
+            p2ImageView.setImage(p2Card.getImage());
 
-        //P1 card value = P2 card value?
-        if (p1Card.getCardValue() == p2Card.getCardValue())
-            playWarHand();
-        else if (p1Card.getCardValue()>p2Card.getCardValue())//player 1 wins the hand
-        {
-            p1Hand.addAll(cardPile);
-            //Clear the center Pile
-            cardPile.clear();
+            //P1 card value = P2 card value?
+            if (p1Card.getCardValue() == p2Card.getCardValue())
+                playWarHand();
+            else if (p1Card.getCardValue()>p2Card.getCardValue())//player 1 wins the hand
+            {
+                p1Hand.addAll(cardPile);
+                //Clear the center Pile
+                cardPile.clear();
+            }
+            else //player 2 wins the hand
+            {
+                p2Hand.addAll(cardPile);
+                //Clear the center Pile
+                cardPile.clear();
+            }
+            updateLabels();
         }
-        else //player 2 wins the hand
+    }
+
+    @FXML
+    private void newGame()
+    {
+        playAgainButton.setDisable(true);
+        playButton.setDisable(false);
+        winnerLabel.setVisible(false);
+        p1ImageView.setVisible(false);
+        p2ImageView.setVisible(false);
+
+        //Create variables for p1, p2 & card pile
+        p1Hand = new ArrayList<>();
+        p2Hand = new ArrayList<>();
+        cardPile = new ArrayList<>();
+
+        //create a deck of cards and shuffle the deck
+        DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+
+        //this was added to reduce the deck size and make testing easier
+//        for (int i=1; i<=46; i++)
+//            deck.dealTopCard();
+
+        //Are all Cards Dealt?
+        while (deck.getNumOfCardsInDeck()>=2)
         {
-            p2Hand.addAll(cardPile);
-            //Clear the center Pile
-            cardPile.clear();
+            p1Hand.add(deck.dealTopCard());
+            p2Hand.add(deck.dealTopCard());
         }
 
+        //designed an experiment to test what is going on with the "war hand"
+//        Card ace = new Card("ace","spades");
+//        p1Hand.add(0,ace);
+//        p2Hand.add(0,ace);
 
         updateLabels();
     }
+
+    @FXML
+    private Button playAgainButton;
 
     /**
      * This method updates the labels to show the status of the game
@@ -107,7 +154,20 @@ public class WarGameController implements Initializable {
 
     private void declareWinner(String winnerName)
     {
-        System.out.println("The winner is "+winnerName);
+        playButton.setDisable(true);
+        playAgainButton.setDisable(false);
+
+        //Make p1 card image invisible
+        p1ImageView.setVisible(false);
+
+        //Make p2 card image invisible
+        p2ImageView.setVisible(false);
+
+        //Make winner label visible and display winner name
+        winnerLabel.setVisible(true);
+        winnerLabel.setText(winnerName + " wins!!");
+
+        updateLabels();
     }
 
     @Override
@@ -115,31 +175,6 @@ public class WarGameController implements Initializable {
         newGame();
     }
 
-    private void newGame()
-    {
-        p1ImageView.setVisible(false);
-        p2ImageView.setVisible(false);
 
-        //Create variables for p1, p2 & card pile
-        p1Hand = new ArrayList<>();
-        p2Hand = new ArrayList<>();
-        cardPile = new ArrayList<>();
-
-        //create a deck of cards and shuffle the deck
-        DeckOfCards deck = new DeckOfCards();
-        deck.shuffle();
-
-        //Are all Cards Dealt?
-        while (deck.getNumOfCardsInDeck()>=2)
-        {
-            p1Hand.add(deck.dealTopCard());
-            p2Hand.add(deck.dealTopCard());
-        }
-
-        //designed an experiment to test what is going on with the "war hand"
-//        Card ace = new Card("ace","spades");
-//        p1Hand.add(0,ace);
-//        p2Hand.add(0,ace);
-    }
 
 }
